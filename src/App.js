a2dd7ad94963f651f53df3038690f85a83164d29
Vignesh <vigneshb2896@gmail.com>
2024-09-28 23:18:51 +0530

    Upgrade, Formatting and Corrections.

--- src/App.js
+++ src/App.js
@@ -1,72 +1,92 @@
-import React, { Component } from 'react';
-import {Overview} from './components/Overview';
+import React, { Component } from "react";
+import { Overview } from "./components/Overview";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      tasks : [],
-    }
+      tasks: [],
+    };
 
     this.onAddSubmit = this.onAddSubmit.bind(this);
     this.onEditSubmit = this.onEditSubmit.bind(this);
-    this.editTaskBtn =  this.editTaskBtn.bind(this);
+    this.editTaskBtn = this.editTaskBtn.bind(this);
     this.formSubmit = this.formSubmit.bind(this);
   }
 
-  editTaskBtn (event) {   // Code Flow for amending a task.
+  editTaskBtn(event) {
+    // Code Flow for amending a task.
     let editPos = event.target.getAttribute("data-pos");
     let editValue = this.state.tasks[editPos]; // Find the task to be updated.
 
-    var newButton = document.getElementById("submit_task_btn").cloneNode(true); // Updating the submit button for the Edit Functionality.
-    var oldButton = document.getElementById("submit_task_btn");
+    let newButton = document.getElementById("submit_task_btn").cloneNode(true); // Updating the submit button for the Edit Functionality.
+    let oldButton = document.getElementById("submit_task_btn");
     oldButton.parentNode.replaceChild(newButton, oldButton);
     newButton.textContent = "Resubmit";
-    newButton.addEventListener("click",this.onEditSubmit);
+    newButton.addEventListener("click", this.onEditSubmit);
 
-    document.getElementById("add_task_input").setAttribute("data-edit-pos", editPos);
+    document
+      .getElementById("add_task_input")
+      .setAttribute("data-edit-pos", editPos);
     document.getElementById("add_task_input").value = editValue; // Updating the Input form for edit Functionality.
     document.getElementById("add_task_input").focus();
   }
 
-  formSubmit (event) {
+  formSubmit(event) {
     event.preventDefault();
   }
 
-  componentDidUpdate() {    // Adding event listener for the Edit Buttons after render.
+  componentDidUpdate() {
+    // Adding event listener for the Edit Buttons after render.
     document.querySelectorAll("#edit_task_btn").forEach((editBtn) => {
-      editBtn.addEventListener("click",this.editTaskBtn);
+      editBtn.addEventListener("click", this.editTaskBtn);
     });
   }
 
-  onEditSubmit () {   // Function to update state with amended task value.
+  onEditSubmit() {
+    // Function to update state with amended task value.
     let currentTaskList = this.state.tasks;
-    let editPos = document.getElementById("add_task_input").getAttribute("data-edit-pos");
-    let editValue  = document.getElementById("add_task_input").value;
+    let editPos = document
+      .getElementById("add_task_input")
+      .getAttribute("data-edit-pos");
+    let editValue = document.getElementById("add_task_input").value;
     currentTaskList[editPos] = editValue;
     this.setState({
-      tasks : currentTaskList,
-    })
+      tasks: currentTaskList,
+    });
     document.getElementById("task_form").reset();
+    document.getElementById("submit_task_btn").textContent = "Submit"
   }
 
-  onAddSubmit () { // Function to add to state with newly added task.
-    var newTaskToAdd = document.getElementById("add_task_input").value;
+  onAddSubmit() {
+    // Function to add to state with newly added task.
+    let newTaskToAdd = document.getElementById("add_task_input").value;
+    let tempTasksList = this.state.tasks;
+    tempTasksList.push(newTaskToAdd);
     this.setState({
-      tasks : this.state.tasks.concat(newTaskToAdd),
+      tasks: tempTasksList,
     });
     document.getElementById("task_form").reset();
   }
 
   render() {
-    return ( // Create the Form with a TextBox and a Submit button.
+    return (
+      // Create the Form with a TextBox and a Submit button.
       <div>
         <h1> To-Do-Task</h1>
-        <form id = "task_form" onSubmit = {this.formSubmit} >
-          <input  type = "text" id = "add_task_input" data-edit-pos = "" placeholder = "Task to be Added" minLength="1"></input>
-          <button type = "Submit" id = "submit_task_btn" onClick = {this.onAddSubmit} >Submit</button>
-          <Overview arrayList = {this.state.tasks}/>
+        <form id="task_form" onSubmit={this.formSubmit}>
+          <input
+            type="text"
+            id="add_task_input"
+            data-edit-pos=""
+            placeholder="Task to be Added"
+            minLength="1"
+          ></input>
+          <button type="Submit" id="submit_task_btn" onClick={this.onAddSubmit}>
+            Submit
+          </button>
+          <Overview arrayList={this.state.tasks} />
         </form>
       </div>
     );
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-function List(props){   // For Each of the tasks, we will prepare a list tag with the Task Content and a Button for Editing it.
-    if(!props.taskList[0]) return "";
-    const newList = props.taskList.map((task,index) => {  //Map function used to modify all the tasks at once.
-       return <li key = {index}> <p>{task}</p> <button id = "edit_task_btn" data-pos = {index}>Edit Task</button> </li>
-    });
-    return newList
-}
-
-function Overview(props){
-    return ( //Return List Array
-        <div>
-            <h1> Tasks Added : </h1>
-            <ul>
-                <List taskList = {props.arrayList} />
-            </ul>
-        </div>
+function List(props) {
+  // For Each of the tasks, we will prepare a list tag with the Task Content and a Button for Editing it.
+  if (!props.taskList[0]) return "";
+  const newList = props.taskList.map((task, index) => {
+    //Map function used to modify all the tasks at once.
+    return (
+      <li key={index}>
+        {" "}
+        <p>{task}</p>{" "}
+        <button id="edit_task_btn" data-pos={index}>
+          Edit Task
+        </button>{" "}
+      </li>
     );
+  });
+  return newList;
 }
 
-export {Overview};
+function Overview(props) {
+  return (
+    //Return List Array
+    <div>
+      <h1> Tasks Added : </h1>
+      <ul>
+        <List taskList={props.arrayList} />
+      </ul>
+    </div>
+  );
+}
+
+Overview.propTypes = {
+  arrayList : PropTypes.array
+}
+
+
+export { Overview };
